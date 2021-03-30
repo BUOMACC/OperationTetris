c5a8bd276430bce5fcf75f52e829ba56e0979bee
Jin <wpsxkxk@naver.com>
2021-03-30 15:29:03 +0900

    design

--- Assets/BlockLogic.cs
+++ Assets/BlockLogic.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BlockLogic : MonoBehaviour
 {
+    bool movable = true;
     float timer = 0f;
 
     // Start is called before the first frame update
@@ -15,28 +16,31 @@ public class BlockLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Update the timer
-        timer += 1 * Time.deltaTime;
-        //Drop
-        if(Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
+        if(movable)
         {
-            gameObject.transform.position -= new Vector3(0, 1, 0);
-            timer = 0;
-        }
-        else if(timer > GameLogic.dropTime)
-        {
-            gameObject.transform.position -= new Vector3(0, 1, 0);
-            timer = 0;
-        }
+            //Update the timer
+            timer += 1 * Time.deltaTime;
+            //Drop
+            if (Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
+            {
+                gameObject.transform.position -= new Vector3(0, 1, 0);
+                timer = 0;
+            }
+            else if (timer > GameLogic.dropTime)
+            {
+                gameObject.transform.position -= new Vector3(0, 1, 0);
+                timer = 0;
+            }
 
-        //Sideways
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position -= new Vector3(1, 0, 0);
-        }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += new Vector3(1, 0, 0);
+            //Sideways
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gameObject.transform.position -= new Vector3(1, 0, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gameObject.transform.position += new Vector3(1, 0, 0);
+            }
         }
     }
 }
