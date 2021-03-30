66ad5c52596d6098135e4e6a79835a4de66e7978
Jin <wpsxkxk@naver.com>
2021-03-30 15:12:51 +0900

    block down(auto, manual)

--- Assets/BlockLogic.cs
+++ Assets/BlockLogic.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BlockLogic : MonoBehaviour
 {
+    float timer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,21 @@ public class BlockLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Update the timer
+        timer += 1 * Time.deltaTime;
+        //Drop
+        if(Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
+        {
+            gameObject.transform.position -= new Vector3(0, 1, 0);
+            timer = 0;
+        }
+        else if(timer > GameLogic.dropTime)
+        {
+            gameObject.transform.position -= new Vector3(0, 1, 0);
+            timer = 0;
+        }
+
+        //Sideways
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             gameObject.transform.position -= new Vector3(1, 0, 0);
---
+++ Assets/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public static float dropTime = 0.9f;
+    public static float quickDropTime = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
