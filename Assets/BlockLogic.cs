62867dfb9cbc8065475a054f92622b181003ea36
Jin <wpsxkxk@naver.com>
2021-04-06 14:58:52 +0900

    rotate(second video)
    https://youtu.be/T5P8ohdxDjo
    참조, 회전점 설정, 회전 깔끔.
    추가할 내용 : 특정 모양인 상태에서 맵 왼쪽 혹은 오른쪽에서 회전 시 회전이 불가능한 현상 해결(강제적으로 오른쪽 혹은 왼쪽으로 한 칸 이동해야 할 듯)

--- Assets/BlockLogic.cs
+++ Assets/BlockLogic.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class BlockLogic : MonoBehaviour
 {
+    GameLogic gameLogic;
     bool movable = true;
     float timer = 0f;
     public GameObject rig;
+    public Vector3 rotationPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameLogic = FindObjectOfType<GameLogic>();
     }
     bool CheckValid()
     {
         foreach (Transform subBlock in rig.transform)
         {
-            if(subBlock.transform.position.x > GameLogic.width ||
+            if(subBlock.transform.position.x >= GameLogic.width ||
                 subBlock.transform.position.x < 0 ||
                 subBlock.transform.position.y < 0)
             {
@@ -41,6 +43,7 @@ public class BlockLogic : MonoBehaviour
                 {
                     movable = false;
                     gameObject.transform.position += new Vector3(0, 1, 0);
+                    gameLogic.SpawnBlock();
                 }
             }
             else if (timer > GameLogic.dropTime)
@@ -72,6 +75,7 @@ public class BlockLogic : MonoBehaviour
                 }
             }
             //Rotation
+            /*
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 rig.transform.eulerAngles -= new Vector3(0, 0, 90);
@@ -79,6 +83,13 @@ public class BlockLogic : MonoBehaviour
                 {
                     rig.transform.eulerAngles += new Vector3(0, 0, 90);
                 }
+            }
+            */
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                if (!CheckValid())
+                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
             }
         }
     }
--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -7,15 +7,23 @@ public class GameLogic : MonoBehaviour
     public static float dropTime = 0.9f;
     public static float quickDropTime = 0.05f;
     public static int width = 15, height = 30;
+    public GameObject[] blocks;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnBlock();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnBlock()
+    {
+        float guess = Random.Range(0, 1f);
+        guess *= blocks.Length;
+        Instantiate(blocks[Mathf.FloorToInt(guess)]);
     }
 }
