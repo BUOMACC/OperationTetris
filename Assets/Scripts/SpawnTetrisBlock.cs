e24767ced93436ce91b33615df81ba0a428d6732
BUOMACC <leehy1235@naver.com>
2021-04-06 15:15:18 +0900

    샘플코드

---
+++ Assets/Scripts/SpawnTetrisBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetrisBlock : MonoBehaviour
+{
+	public GameObject[] blocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		NewTetrisBlock();
+	}
+
+	public void NewTetrisBlock()
+	{
+		Instantiate(blocks[Random.Range(0, blocks.Length)], transform.position, Quaternion.identity);
+	}
+}
