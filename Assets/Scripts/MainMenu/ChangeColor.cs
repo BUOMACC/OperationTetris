92070f68fd5ff1640b7e21fb459a64b108bcdb3e
BUOMACC <leehy1235@naver.com>
2021-04-21 20:47:16 +0900

    UI개발

---
+++ Assets/Scripts/MainMenu/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChangeColor : MonoBehaviour
+{
+	public FastMobileBloom bloom;
+	public float speed = 1.0f;
+
+	int dir = 1;
+
+	void Update()
+	{
+		bloom.intensity += speed * Time.deltaTime * dir;
+
+		if (bloom.intensity >= 4) dir = -1;
+		else if (bloom.intensity <= 0) dir = 1;
+	}
+}
---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+		Screen.SetResolution(720, 1280, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
