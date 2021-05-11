8662c2c0c6cbf5232b38cb2b1a4c77fda33f2de3
BUOMACC <leehy1235@naver.com>
2021-05-11 14:07:36 +0900

    DB 코드 틀 제작

---
+++ Assets/Scripts/DBManager/AccountManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AccountManager : MonoBehaviour
+{
+	public UnityEvent OnLoginSuccess;
+	public UnityEvent OnLoginFail;
+	public UnityEvent OnRegisterSuccess;
+	public UnityEvent OnRegisterFail;
+
+	public void TryLogin(string id, string pw)
+	{
+		StartCoroutine(LoginCoroutine(id, pw));
+	}
+
+	IEnumerator LoginCoroutine(string id, string pw)
+	{
+		/* 로그인 성공시
+		  OnLoginSuccess.Invoke(); 호출
+		실패시 OnLoginFail.Invoke(); 호출
+		*/
+		yield return null;
+	}
+
+	public void TryRegister(string id, string pw)
+	{
+		StartCoroutine(RegisterCoroutine(id, pw));
+	}
+
+	IEnumerator RegisterCoroutine(string id, string pw)
+	{
+		/* 로그인 성공시
+		  OnRegisterSuccess.Invoke(); 호출
+		실패시 OnRegisterFail.Invoke(); 호출
+		*/
+		yield return null;
+	}
+}
